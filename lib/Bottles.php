05ef63bfa3cf1042096c25f186e83359681b33cd
Katrina Owen <kytrinyx@github.com>
2020-08-02 12:26:54 -0600

    Send successor from default case, phrase 4

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -31,7 +31,7 @@ class Bottles {
           "of milk on the wall, " .
         "{$this->quantity($number)} {$this->container($number)} of milk.\n" .
         "{$this->action($number)}, " .
-        "{$this->quantity($number-1)} {$this->container($number-1)} " .
+        "{$this->quantity($this->successor($number))} {$this->container($number-1)} " .
           "of milk on the wall.\n";
     }
   }
