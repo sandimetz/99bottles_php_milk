93be26f6458df48865f3b0cbcca30a8015405c26
Katrina Owen <kytrinyx@github.com>
2020-08-20 08:09:09 -0600

    Add bottleNumberFor

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -26,6 +26,10 @@ class Bottles {
       "{$bottleNumber->action()}, " .
       "{$nextBottleNumber} of milk on the wall.\n";
   }
+
+  public function bottleNumberFor($number) {
+    return new BottleNumber($number);
+  }
 }
 
 class BottleNumber {
