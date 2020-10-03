b4b52ee531825d220b2a5a95d9d21e144b982bfb
Katrina Owen <kytrinyx@github.com>
2020-10-03 14:10:31 -0600

    Make test for verse 99 pass

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -2,5 +2,10 @@
 
 class Bottles {
   public function verse() {
+    return
+      "99 bottles of milk on the wall, " .
+      "99 bottles of milk.\n" .
+      "Take one down and pass it around, " .
+      "98 bottles of milk on the wall.\n";
   }
 }
