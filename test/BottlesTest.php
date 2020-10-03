812fd486d9ee1cfab1bedd0ea983b257cd4a2403
Katrina Owen <kytrinyx@github.com>
2020-10-03 14:04:58 -0600

    Add test for verse 99
    This fails.

--- test/BottlesTest.php
+++ test/BottlesTest.php
@@ -3,4 +3,12 @@
 require __DIR__ . "/../lib/Bottles.php";
 
 class BottlesTest extends \PHPUnit\Framework\TestCase {
+  public function test_the_first_verse() {
+    $expected =
+      "99 bottles of milk on the wall, " .
+      "99 bottles of milk.\n" .
+      "Take one down and pass it around, " .
+      "98 bottles of milk on the wall.\n";
+    $this->assertEquals($expected, (new Bottles())->verse(99));
+  }
 }
