48ee335749603b6733e5285efb50ee7d7c6e181b
Katrina Owen <kytrinyx@github.com>
2020-10-10 12:48:08 -0600

    Revert back to old verse

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -15,15 +15,13 @@ class Bottles {
   }
 
   public function verse(int $number): string {
-    return (new BottleVerse($number))->verse($number);
+    $bottleNumber = BottleNumber::for($number);
 
-    // $bottleNumber = BottleNumber::for($number);
-
-    // return
-    //   ucfirst("{$bottleNumber} of milk on the wall, ") .
-    //   "{$bottleNumber} of milk.\n" .
-    //   "{$bottleNumber->action()}, " .
-    //   "{$bottleNumber->successor()} of milk on the wall.\n";
+    return
+      ucfirst("{$bottleNumber} of milk on the wall, ") .
+      "{$bottleNumber} of milk.\n" .
+      "{$bottleNumber->action()}, " .
+      "{$bottleNumber->successor()} of milk on the wall.\n";
   }
 }
 
