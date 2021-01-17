sandimetz/99bottles_php

Put phrase 1 on one line

lib/Bottles.php
@@ -19,8 +19,7 @@ class Bottles {
     $nextBottleNumber = new BottleNumber($bottleNumber->successor());
 
     return
-      ucfirst($bottleNumber->toString()) .
-        " of beer on the wall, " .
+      ucfirst($bottleNumber->toString()) . " of beer on the wall, " .
       $bottleNumber->toString() . " of beer.\n" .
       $bottleNumber->action() . ", " .
       $nextBottleNumber->toString() .

sandimetz/99bottles_php_milk

lib/Bottles.php
@@ -19,8 +19,7 @@ class Bottles {
     $nextBottleNumber = new BottleNumber($bottleNumber->successor());
 
     return
-      ucfirst($bottleNumber->toString()) .
-        " of milk on the wall, " .
+      ucfirst($bottleNumber->toString()) . " of milk on the wall, " .
       $bottleNumber->toString() . " of milk.\n" .
       $bottleNumber->action() . ", " .
       $nextBottleNumber->toString() .