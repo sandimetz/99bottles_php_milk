sandimetz/99bottles_php

Copy 1st test from BottlesTest to BottleVerseTest

test/BottleVerseTest.php
@@ -3,4 +3,12 @@
 require_once __DIR__ . "/../lib/Bottles.php";
 
 class BottleVerseTest extends \PHPUnit\Framework\TestCase {
+  public function test_the_first_verse() {
+    $expected =
+      "99 bottles of beer on the wall, " .
+      "99 bottles of beer.\n" .
+      "Take one down and pass it around, " .
+      "98 bottles of beer on the wall.\n";
+    $this->assertEquals($expected, (new Bottles())->verse(99));
+  }
 }

sandimetz/99bottles_php_milk

test/BottleVerseTest.php
@@ -3,4 +3,12 @@
 require_once __DIR__ . "/../lib/Bottles.php";
 
 class BottleVerseTest extends \PHPUnit\Framework\TestCase {
+  public function test_the_first_verse() {
+    $expected =
+      "99 bottles of milk on the wall, " .
+      "99 bottles of milk.\n" .
+      "Take one down and pass it around, " .
+      "98 bottles of milk on the wall.\n";
+    $this->assertEquals($expected, (new Bottles())->verse(99));
+  }
 }