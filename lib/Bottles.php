sandimetz/99bottles_php

Add bottleNumberFor method

lib/Bottles.php
@@ -24,6 +24,10 @@ class Bottles {
       "{$bottleNumber->action()}, " .
       "{$nextBottleNumber} of beer on the wall.\n";
   }
+
+  public function bottleNumberFor(int $number): object {
+    return new BottleNumber($number);
+  }
 }
 
 class BottleNumber {

sandimetz/99bottles_php_milk

lib/Bottles.php
@@ -24,6 +24,10 @@ class Bottles {
       "{$bottleNumber->action()}, " .
       "{$nextBottleNumber} of milk on the wall.\n";
   }
+
+  public function bottleNumberFor(int $number): BottleNumber {
+    return new BottleNumber($number);
+  }
 }
 
 class BottleNumber {