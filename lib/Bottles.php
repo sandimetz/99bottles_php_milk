sandimetz/99bottles_php

Send toString in phrase 2

lib/Bottles.php
@@ -21,7 +21,7 @@ class Bottles {
     return
       ucfirst($bottleNumber->toString()) .
         " of beer on the wall, " .
-      $bottleNumber->quantity() . " " . $bottleNumber->container() . " of beer.\n" .
+      $bottleNumber->toString() . " of beer.\n" .
       $bottleNumber->action() . ", " .
       $nextBottleNumber->quantity() . " " . $nextBottleNumber->container() .
         " of beer on the wall.\n";

sandimetz/99bottles_php_milk

lib/Bottles.php
@@ -21,7 +21,7 @@ class Bottles {
     return
       ucfirst($bottleNumber->toString()) .
         " of milk on the wall, " .
-      $bottleNumber->quantity() . " " . $bottleNumber->container() . " of milk.\n" .
+      $bottleNumber->toString() . " of milk.\n" .
       $bottleNumber->action() . ", " .
       $nextBottleNumber->quantity() . " " . $nextBottleNumber->container() .
         " of milk on the wall.\n";