sandimetz/99bottles_php

Revert back to old verse

lib/Bottles.php
@@ -15,15 +15,13 @@ class Bottles {
   }
 
   public function verse(int $number): string {
-    return (new BottleVerse($number))->verse($number);
+    $bottleNumber = BottleNumber::for($number);
 
-    // $bottleNumber = BottleNumber::for($number);
-
-    // return
-    //   ucfirst("{$bottleNumber} of beer on the wall, ") .
-    //   "{$bottleNumber} of beer.\n" .
-    //   "{$bottleNumber->action()}, " .
-    //   "{$bottleNumber->successor()} of beer on the wall.\n";
+    return
+      ucfirst("{$bottleNumber} of beer on the wall, ") .
+      "{$bottleNumber} of beer.\n" .
+      "{$bottleNumber->action()}, " .
+      "{$bottleNumber->successor()} of beer on the wall.\n";
   }
 }
 

sandimetz/99bottles_php_milk

lib/Bottles.php
@@ -15,15 +15,13 @@ class Bottles {
   }
 
   public function verse(int $number): string {
-    return (new BottleVerse($number))->verse($number);
+    $bottleNumber = BottleNumber::for($number);
 
-    // $bottleNumber = BottleNumber::for($number);
-
-    // return
-    //   ucfirst("{$bottleNumber} of milk on the wall, ") .
-    //   "{$bottleNumber} of milk.\n" .
-    //   "{$bottleNumber->action()}, " .
-    //   "{$bottleNumber->successor()} of milk on the wall.\n";
+    return
+      ucfirst("{$bottleNumber} of milk on the wall, ") .
+      "{$bottleNumber} of milk.\n" .
+      "{$bottleNumber->action()}, " .
+      "{$bottleNumber->successor()} of milk on the wall.\n";
   }
 }
 