sandimetz/99bottles_php

Use implicit cast in phrase 2 with interpolation

lib/Bottles.php
@@ -20,7 +20,7 @@ class Bottles {
 
     return
       ucfirst("{$bottleNumber} of beer on the wall, ") .
-      (string)$bottleNumber . " of beer.\n" .
+      "{$bottleNumber} of beer.\n" .
       $bottleNumber->action() . ", " .
       (string)$nextBottleNumber . " of beer on the wall.\n";
   }

sandimetz/99bottles_php_milk

lib/Bottles.php
@@ -20,7 +20,7 @@ class Bottles {
 
     return
       ucfirst("{$bottleNumber} of milk on the wall, ") .
-      (string)$bottleNumber . " of milk.\n" .
+      "{$bottleNumber} of milk.\n" .
       $bottleNumber->action() . ", " .
       (string)$nextBottleNumber . " of milk on the wall.\n";
   }