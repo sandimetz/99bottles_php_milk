sandimetz/99bottles_php

Replace toString invocations with explicit cast

lib/Bottles.php
@@ -19,10 +19,10 @@ class Bottles {
     $nextBottleNumber = new BottleNumber($bottleNumber->successor());
 
     return
-      ucfirst($bottleNumber->toString()) . " of beer on the wall, " .
-      $bottleNumber->toString() . " of beer.\n" .
+      ucfirst((string)$bottleNumber) . " of beer on the wall, " .
+      (string)$bottleNumber . " of beer.\n" .
       $bottleNumber->action() . ", " .
-      $nextBottleNumber->toString() . " of beer on the wall.\n";
+      (string)$nextBottleNumber . " of beer on the wall.\n";
   }
 }
 

sandimetz/99bottles_php_milk

lib/Bottles.php
@@ -19,10 +19,10 @@ class Bottles {
     $nextBottleNumber = new BottleNumber($bottleNumber->successor());
 
     return
-      ucfirst($bottleNumber->toString()) . " of milk on the wall, " .
-      $bottleNumber->toString() . " of milk.\n" .
+      ucfirst((string)$bottleNumber) . " of milk on the wall, " .
+      (string)$bottleNumber . " of milk.\n" .
       $bottleNumber->action() . ", " .
-      $nextBottleNumber->toString() . " of milk on the wall.\n";
+      (string)$nextBottleNumber . " of milk on the wall.\n";
   }
 }
 