sandimetz/99bottles_php

Add parameter type declaration back to ::for

lib/Bottles.php
@@ -28,7 +28,7 @@ class Bottles {
 class BottleNumber {
   protected $number;
 
-  public static function for($number): object {
+  public static function for(int $number): object {
     switch ($number) {
     case 0:
       $className = BottleNumber0::class;

sandimetz/99bottles_php_milk

lib/Bottles.php
@@ -28,7 +28,7 @@ class Bottles {
 class BottleNumber {
   protected $number;
 
-  public static function for($number): BottleNumber {
+  public static function for(int $number): BottleNumber {
     switch ($number) {
     case 0:
       $className = BottleNumber0::class;