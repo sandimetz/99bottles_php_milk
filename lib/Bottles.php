sandimetz/99bottles_php

Temporarily remove parameter type declaration or ::for factory

lib/Bottles.php
@@ -30,7 +30,7 @@ class Bottles {
 class BottleNumber {
   protected $number;
 
-  public static function for(int $number): object {
+  public static function for($number): object {
     switch ($number) {
     case 0:
       $className = BottleNumber0::class;

sandimetz/99bottles_php_milk

lib/Bottles.php
@@ -30,7 +30,7 @@ class Bottles {
 class BottleNumber {
   protected $number;
 
-  public static function for(int $number): BottleNumber {
+  public static function for($number): BottleNumber {
     switch ($number) {
     case 0:
       $className = BottleNumber0::class;