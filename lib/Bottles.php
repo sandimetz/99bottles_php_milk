sandimetz/99bottles_php

Call successor on bottleNumber in phrase 4

lib/Bottles.php
@@ -22,7 +22,7 @@ class Bottles {
       ucfirst("{$bottleNumber} of beer on the wall, ") .
       "{$bottleNumber} of beer.\n" .
       "{$bottleNumber->action()}, " .
-      "{$nextBottleNumber} of beer on the wall.\n";
+      "{$bottleNumber->successor()} of beer on the wall.\n";
   }
 }
 

sandimetz/99bottles_php_milk

lib/Bottles.php
@@ -22,7 +22,7 @@ class Bottles {
       ucfirst("{$bottleNumber} of milk on the wall, ") .
       "{$bottleNumber} of milk.\n" .
       "{$bottleNumber->action()}, " .
-      "{$nextBottleNumber} of milk on the wall.\n";
+      "{$bottleNumber->successor()} of milk on the wall.\n";
   }
 }
 