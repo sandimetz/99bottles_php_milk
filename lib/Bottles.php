sandimetz/99bottles_php

Use cached bottleNumber in phrase 2

lib/Bottles.php
@@ -20,7 +20,7 @@ class Bottles {
     return
       ucfirst($bottleNumber->quantity()) . " " . $bottleNumber->container() .
         " of beer on the wall, " .
-      $this->quantity($number) . " " . $this->container($number) . " of beer.\n" .
+      $bottleNumber->quantity() . " " . $bottleNumber->container() . " of beer.\n" .
       $bottleNumber->action() . ", " .
       $this->quantity($this->successor($number)) . " " .
         $this->container($this->successor($number)) . " of beer on the wall.\n";

sandimetz/99bottles_php_milk

lib/Bottles.php
@@ -20,7 +20,7 @@ class Bottles {
     return
       ucfirst($bottleNumber->quantity()) . " " . $bottleNumber->container() .
         " of milk on the wall, " .
-      $this->quantity($number) . " " . $this->container($number) . " of milk.\n" .
+      $bottleNumber->quantity() . " " . $bottleNumber->container() . " of milk.\n" .
       $bottleNumber->action() . ", " .
       $this->quantity($this->successor($number)) . " " .
         $this->container($this->successor($number)) . " of milk on the wall.\n";