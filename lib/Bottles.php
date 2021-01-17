sandimetz/99bottles_php

Put phrase 4 on one line

lib/Bottles.php
@@ -22,8 +22,7 @@ class Bottles {
       ucfirst($bottleNumber->toString()) . " of beer on the wall, " .
       $bottleNumber->toString() . " of beer.\n" .
       $bottleNumber->action() . ", " .
-      $nextBottleNumber->toString() .
-        " of beer on the wall.\n";
+      $nextBottleNumber->toString() . " of beer on the wall.\n";
   }
 }
 

sandimetz/99bottles_php_milk

lib/Bottles.php
@@ -22,8 +22,7 @@ class Bottles {
       ucfirst($bottleNumber->toString()) . " of milk on the wall, " .
       $bottleNumber->toString() . " of milk.\n" .
       $bottleNumber->action() . ", " .
-      $nextBottleNumber->toString() .
-        " of milk on the wall.\n";
+      $nextBottleNumber->toString() . " of milk on the wall.\n";
   }
 }
 