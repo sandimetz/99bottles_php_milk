sandimetz/99bottles_php

Copy all other verse tests from BottlesTest to BottleVerseTest

test/BottleVerseTest.php
@@ -11,4 +11,40 @@ class BottleVerseTest extends \PHPUnit\Framework\TestCase {
       "98 bottles of beer on the wall.\n";
     $this->assertEquals($expected, BottleVerse::lyrics(99));
   }
+
+  public function test_another_verse() {
+    $expected =
+      "3 bottles of beer on the wall, " .
+      "3 bottles of beer.\n" .
+      "Take one down and pass it around, " .
+      "2 bottles of beer on the wall.\n";
+    $this->assertEquals($expected, (new Bottles())->verse(3));
+  }
+
+  public function test_verse_2() {
+    $expected =
+      "2 bottles of beer on the wall, " .
+      "2 bottles of beer.\n" .
+      "Take one down and pass it around, " .
+      "1 bottle of beer on the wall.\n";
+    $this->assertEquals($expected, (new Bottles())->verse(2));
+  }
+
+  public function test_verse_1() {
+    $expected =
+      "1 bottle of beer on the wall, " .
+      "1 bottle of beer.\n" .
+      "Take it down and pass it around, " .
+      "no more bottles of beer on the wall.\n";
+    $this->assertEquals($expected, (new Bottles())->verse(1));
+  }
+
+  public function test_verse_0() {
+    $expected =
+      "No more bottles of beer on the wall, " .
+      "no more bottles of beer.\n" .
+      "Go to the store and buy some more, " .
+      "99 bottles of beer on the wall.\n";
+    $this->assertEquals($expected, (new Bottles())->verse(0));
+  }
 }

sandimetz/99bottles_php_milk

test/BottleVerseTest.php
@@ -11,4 +11,40 @@ class BottleVerseTest extends \PHPUnit\Framework\TestCase {
       "98 bottles of milk on the wall.\n";
     $this->assertEquals($expected, BottleVerse::lyrics(99));
   }
+
+  public function test_another_verse() {
+    $expected =
+      "3 bottles of milk on the wall, " .
+      "3 bottles of milk.\n" .
+      "Take one down and pass it around, " .
+      "2 bottles of milk on the wall.\n";
+    $this->assertEquals($expected, (new Bottles())->verse(3));
+  }
+
+  public function test_verse_2() {
+    $expected =
+      "2 bottles of milk on the wall, " .
+      "2 bottles of milk.\n" .
+      "Take one down and pass it around, " .
+      "1 bottle of milk on the wall.\n";
+    $this->assertEquals($expected, (new Bottles())->verse(2));
+  }
+
+  public function test_verse_1() {
+    $expected =
+      "1 bottle of milk on the wall, " .
+      "1 bottle of milk.\n" .
+      "Take it down and pass it around, " .
+      "no more bottles of milk on the wall.\n";
+    $this->assertEquals($expected, (new Bottles())->verse(1));
+  }
+
+  public function test_verse_0() {
+    $expected =
+      "No more bottles of milk on the wall, " .
+      "no more bottles of milk.\n" .
+      "Go to the store and buy some more, " .
+      "99 bottles of milk on the wall.\n";
+    $this->assertEquals($expected, (new Bottles())->verse(0));
+  }
 }